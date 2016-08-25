0352235e6448a553da498123358bced12dc643fc
nwebz <nweber522@gmail.com>
2016-08-25 17:50:53 -0400

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# property-tax-delinquency-pipeline
+Python workflow for property tax data transformation 
+
+
+```bash
+cat input/sample.txt | tax_clean.py > output/clean.csv
+
+```
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='property-tax-delinquent-pipeline',
+    version='1.0.0',
+    packages=['phl_delinquent'],
+    scripts=['tax_clean.py']
+    )
